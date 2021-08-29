b08561168edb9407ce082099d51a8d7615e93c0a
kirenz <jan.kirenz@outlook.de>
2021-08-29 22:33:42 +0200

    added tutorials

--- 01-first-app/app.py
+++ 01-first-app/app.py
@@ -51,6 +51,9 @@ app.layout = html.Div([
         id='example-graph',
         figure=fig
     )
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/5BAthiN0htc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
 ])
 
 
